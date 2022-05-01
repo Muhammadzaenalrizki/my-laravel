f1ea9495380a13445e4c28f38322a30972475554
Muhammad Zaenal <muhammadzaenal855@gmail.com>
2022-05-01 13:49:28 +0700

    first testing

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -4,18 +4,31 @@ namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
+use App\Box;
+
+// assertTrue() ->return yang di kembalikan harus true
+// assertFalse()->return yang di kembalikan harus false
+// assertEquals()->akan membandingkan nilai yang di pass dan yang di kembalian(nilai yang dikembalikan harus sama)
+// assertNull()->nilai yang di kembalikan harus null
+// assertContains()
+// assertCount()
+// assertEmpty()
+// assertStatus()
 
 class ExampleTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function test_example()
+    # Test function for Box class
+    public function test_BoxContents()
     {
-        $response = $this->get('/');
-
-        $response->assertStatus(200);
+        $box = new Box(['toy']);
+        $this->assertTrue($box->has('toy'));
+        $this->assertFalse($box->has('ball'));
+    }
+    public function testTakeOneFromTheBox()
+    {
+        $box = new Box(['torch']);
+        $this->assertEquals('torch', $box->takeOne());
+        // Null, now the box is empty
+        $this->assertNull($box->takeOne());
     }
 }
